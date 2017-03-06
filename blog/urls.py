6f0880d367873700c25a38dfaecfd99d079dec4c
程序小白猿 <15301072780@163.com>
2017-03-06 21:46:23 +0800

    博客主页面搭建完毕

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views as bv
 
 urlpatterns = [
     url(r'^index/$',bv.index),
+    url(r'^article/$',bv.article_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 import  models
 
-def index(request):
+def index0(request):
     article = models.Article.objects.get(pk=1)
     return render(request, "blog/index.html", {"article": article})
 
+def index(request):
+    articles = models.Article.objects.all()
+    return render(request,"blog/index.html", {"articles": articles})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,"blog/article_page.html",{"article":article})
