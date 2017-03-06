7d591a3059b1fae7b0463650a2c7a7a6d88ee1cd
程序小白猿 <15301072780@163.com>
2017-03-06 17:29:21 +0800

    为blog应用制作数据迁移

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=32,default='title')
+    content  = models.TextField(null=True)
