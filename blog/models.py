4344a2b097e26b7ae49ed156a9cf33a6b9ce1025
程序小白猿 <15301072780@163.com>
2017-03-06 18:23:12 +0800

    修改控制台数据默认显示方式

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32,default='title')
     content  = models.TextField(null=True)
+
+    def __unicode__(self):
+        return  self.title
