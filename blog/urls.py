5b3c953df3bb4a0810504b8d42c12dc15a33bd74
程序小白猿 <15301072780@163.com>
2017-03-07 11:01:11 +0800

    实现撰写博客功能

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views as bv
 
 urlpatterns = [
-    url(r'^index/$',bv.index),
-    url(r'^article/(?P<article_id>[0-9]+)$',bv.article_page,name="article_page"),
+    url(r'^index/$', bv.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', bv.article_page,name="article_page"),
+    url(r'^edit/$', bv.edit_page,name="edit_page"),
+    url(r'^edit/action/$', bv.edit_action,name="edit_action"),
 ]
