2b73ae47f15e8fdeacb20710c4c3b86d437529e6
程序小白猿 <15301072780@163.com>
2017-03-07 11:39:11 +0800

    修改撰写页面，复用为文章修改页面

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views as bv
 urlpatterns = [
     url(r'^index/$', bv.index),
     url(r'^article/(?P<article_id>[0-9]+)$', bv.article_page,name="article_page"),
-    url(r'^edit/$', bv.edit_page,name="edit_page"),
+    url(r'^edit/(?P<article_id>[0-9]+)$', bv.edit_page,name="edit_page"),
     url(r'^edit/action/$', bv.edit_action,name="edit_action"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,8 +12,12 @@ def index(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,"blog/article_page.html",{"article":article})
-def edit_page(request):
-    return  render(request,"blog/edit_page.html")
+
+def edit_page(request,article_id):
+    if str(article_id) == "0":
+        return  render(request,"blog/edit_page.html")
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, "blog/edit_page.html",{"article":article })
 
 def edit_action(request):
     title = request.POST.get("title")
