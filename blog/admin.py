fff06c67e2798b01c1dc97b13a440af160372cc6
程序小白猿 <15301072780@163.com>
2017-03-07 14:42:37 +0800

    番外篇：admin增加显示字段已经筛选功能

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from models import  Article
+from models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title","content","pub_time")
+    list_filter = ("pub_time",)
+
+admin.site.register(Article,ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import django.utils.timezone as timezone
 
 class Article(models.Model):
     title = models.CharField(max_length=32,default='title')
-    content  = models.TextField(null=True)
+    content = models.TextField(null=True)
+    pub_time = models.DateTimeField(auto_now=True)
+
 
     def __unicode__(self):
         return  self.title
